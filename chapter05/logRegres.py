871075c27fc5638e430360252316afba3ed959d2
WinDaLex <windalex6@gmail.com>
2014-10-08 22:52:59 +0800

    Modify logRegres.py

--- chapter05/logRegres.py
+++ chapter05/logRegres.py
@@ -22,5 +22,29 @@ def gradAscent(dataMatIn, classLabels):
     for k in range(maxCycles):
         h = sigmoid(dataMatrix * weights)
         error = labelMat - h
-        weights += alpha * dataMatrix.transpose() * error
+        weights = weights + alpha * dataMatrix.transpose() * error
     return weights
+
+def plotBestFit(weight):
+    import matplotlib.pyplot as plot
+    weights = weight.getA()
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1]); ycord1.append(dataArr[i, 2]);
+        else:
+            xcord2.append(dataArr[i, 1]); ycord2.append(dataArr[i, 2]);
+    figure = plot.figure()
+    ax = figure.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x) / weights[2]
+    ax.plot(x, y)
+    plot.xlabel('X1'); plot.ylabel('X2');
+    plot.show()
+
