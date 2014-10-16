a2bb5a063d91cfb7ecb0e266704d18d779668397
WinDaLex <windalex6@gmail.com>
2014-10-16 17:08:51 +0800

    Complete the chapter08

--- chapter08/regression.py
+++ chapter08/regression.py
@@ -70,4 +70,34 @@ def ridgeTest(xArr, yArr):
         wMat[i,:] = ws.T
     return wMat
 
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = mean(inMat, 0)
+    inVar = var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+def stageWise(xArr, yArr, eps=0.01, numIt=100):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    returnMat = zeros((numIt, n))
+    ws = zeros((n, 1)); wsTest = ws.copy(); wsMax = ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i,:] = ws.T
+    return returnMat
 
